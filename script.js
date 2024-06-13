6cec764735b618a98466c511c6cd3fa508a07432
maheshbabu1807 <164387227+maheshbabu1807@users.noreply.github.com>
2024-06-13 20:33:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Welcome to Tic Tac Toe")
-let music = new Audio("music.mp3")
-let audioTurn = new Audio("ting.mp3")
-let gameover = new Audio("gameover.mp3")
+let music = new Audio("{{ url_for('static', filename='music.mp3') }}")
+let audioTurn = new Audio("{{ url_for('static', filename='ting.mp3') }}")
+let gameover = new Audio("{{ url_for('static', filename='gameover.mp3') }}")
 let turn = "X"
 let isgameover = false;
 
@@ -30,6 +30,7 @@ const checkWin = ()=>{
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
             document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
             document.querySelector(".line").style.width = "20vw";
+            gameover.play();
         }
     })
 }
@@ -59,8 +60,9 @@ reset.addEventListener('click', ()=>{
         element.innerText = ""
     });
     turn = "X"; 
-    isgameover = false
-    document.querySelector(".line").style.width = "0vw";
+    isgameover = false;
+    document.querySelector(".line").style.width = "0";
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0";
     document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
+    music.play();
 })
